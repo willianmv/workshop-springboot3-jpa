901ac0d88de6c59766c4cc6bfe1dd92245778372
willianmv <willianvmarques300@gmail.com>
2025-02-11 20:39:21 -0300

    User entity and resource

---
+++ src/main/java/com/simpleapi/example/controller/UserResource.java
@@ -0,0 +1,18 @@
+package com.simpleapi.example.controller;
+
+import com.simpleapi.example.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "(78)9999-9999", "123456789");
+        return ResponseEntity.ok().body(u);
+    }
+}
