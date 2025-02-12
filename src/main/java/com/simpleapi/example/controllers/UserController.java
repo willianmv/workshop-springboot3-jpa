6feb5b865c6479aa4dc7df1cb1fdf57b4acd59ea
willianmv <willianvmarques300@gmail.com>
2025-02-11 21:26:17 -0300

    Service layer, component registration

---
+++ src/main/java/com/simpleapi/example/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.simpleapi.example.controllers;
+
+import com.simpleapi.example.entities.User;
+import com.simpleapi.example.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User userById = userService.findById(id);
+        return ResponseEntity.ok().body(userById);
+    }
+}
