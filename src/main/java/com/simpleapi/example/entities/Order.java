743328b3fc9da6897e464e4892db83ccddf80dd7
willianmv <willianvmarques300@gmail.com>
2025-02-11 22:51:09 -0300

    Order status enum

--- src/main/java/com/simpleapi/example/entities/Order.java
+++ src/main/java/com/simpleapi/example/entities/Order.java
@@ -1,7 +1,7 @@
 package com.simpleapi.example.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.simpleapi.example.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.time.Instant;
@@ -18,15 +18,18 @@ public class Order {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "cliente_id")
     private User client;
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        this.setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -52,6 +55,16 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getocde();
+        }
     }
 
     @Override
